d87e0ece2621757d9f4ee24e8a87b1c2d0647c7b
Marcelo Herrera <marceloherrera87@gmail.com>
2017-01-22 10:10:01 -0300

    Fix colores paginación

--- estilos.php
+++ estilos.php
@@ -9,8 +9,11 @@ body {background-color:<?php echo $mhmovies_lite['color-fondo']; ?>;}
 <?php if ($mhmovies_lite['color-principal'] != '') { ?>
 .navbar-inverse .navbar-inverse .navbar-nav > .active > a, .navbar-inverse .navbar-nav > .active > a:hover, .navbar-inverse .navbar-nav > .active > a:focus {background-color: <?php echo $mhmovies_lite['color-principal']; ?>;}
 a {color: <?php echo $mhmovies_lite['color-principal']; ?>;}
-a:hover, a:focus {color: <?php echo $mhmovies_lite['color-principal']; ?>;}
-.navbar-inverse .navbar-nav > .active > a, .navbar-inverse .navbar-nav > .active > a:hover, .navbar-inverse .navbar-nav > .active > a:focus {background-color: <?php echo $mhmovies_lite['color-principal']; ?>;}
+a:hover, a:focus {color: <?php echo $mhmovies_lite['color-secundario']; ?>;}
+.navbar-inverse .navbar-nav > .active > a, .navbar-inverse .navbar-nav > .active > a:hover, .navbar-inverse .navbar-nav > .active > a:focus {background-color: <?php echo $mhmovies_lite['color-secundario']; ?>;}
+.pagination > .active > a, .pagination > .active > span, .pagination > .active > a:hover, .pagination > .active > span:hover, .pagination > .active > a:focus, .pagination > .active > span:focus {background-color: <?php echo $mhmovies_lite['color-principal']; ?>;}
+.pagination > li > a, .pagination > li > span {background-color: <?php echo $mhmovies_lite['color-principal']; ?>;}
+.pagination > li > a:hover, .pagination > li > span:hover, .pagination > li > a:focus, .pagination > li > span:focus {background-color: <?php echo $mhmovies_lite['color-secundario']; ?>;}
 <?php } ?>
 <?php if ($mhmovies_lite['color-secundario'] != '') { ?>
 .navbar-inverse .navbar-brand:hover, .navbar-inverse .navbar-brand:focus {color: <?php echo $mhmovies_lite['color-secundario']; ?>;}
